c9b644f3b229c2614a675815145e8e625d2086f0
August Zinsser <august@tinyfunstudios.com>
2013-12-24 16:22:44 -0800

    Simple native call working

---
+++ UnityDemoApp/ArbiterExample/Assets/Entrypoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entrypoint : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log( "Started the Unity test app." );
+		NativeBinding.Foo();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityDemoApp/ArbiterExample/Assets/Plugins/NativeBinding.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class NativeBinding {
+	
+	public static void Foo() {
+#if UNITY_IOS && !UNITY_EDITOR
+		foo("Hi from Unity");
+#else
+		Debug.Log( "Call was made to native function. Has no effect in editor." );
+#endif
+	}
+	
+	
+	[DllImport ("__Internal")]
+	private static extern float foo(string msg);
+	
+}

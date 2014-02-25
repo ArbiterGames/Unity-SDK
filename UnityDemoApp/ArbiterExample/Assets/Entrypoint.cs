e5d7ba6b854e810fc2cc44d7331a4659ea61742b
August Zinsser <august@tinyfunstudios.com>
2014-02-25 15:03:56 -0800

    Added another scene

--- UnityDemoApp/ArbiterExample/Assets/Entrypoint.cs
+++ UnityDemoApp/ArbiterExample/Assets/Entrypoint.cs
@@ -64,6 +64,8 @@ public class Entrypoint : MonoBehaviour {
         Arbiter.AddWalletListener( SomeOtherHandler );
 
         ArbiterDoTheseAsOftenAsYouWant();
+
+        Application.LoadLevel( "SecondScene" );
     }
     
     
---
+++ UnityDemoApp/ArbiterExample/Assets/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class Main : MonoBehaviour {
+	
+	void Start () {
+        Debug.Log("ttt got to main");
+    }
+
+}

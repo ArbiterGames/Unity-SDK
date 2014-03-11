8744d7de491b6b16bc29a6df5ce6c7e18a1351d9
August Zinsser <august@tinyfunstudios.com>
2014-03-11 13:18:16 -0700

    Stubbing in flow for reporting the score

--- UnityDemoApp/ArbiterExample/Assets/FakeGame/Game.cs
+++ UnityDemoApp/ArbiterExample/Assets/FakeGame/Game.cs
@@ -22,19 +22,24 @@ public class Game : MonoBehaviour {
         string buyIn = "0.0001";
         Dictionary<string,string> filters = new Dictionary<string,string>();
         filters.Add( "arbitrary_key", "the_value" );
-        Arbiter.RequestCompetition( buyIn, filters, ResetPolling );
-    }
-
-
-    private void ResetPolling() {
-        Debug.LogWarning("ttt successful request! Start polling for results??");
-        // TODO: Start polling for the previous game states
+        Arbiter.RequestCompetition( buyIn, filters, PlayGame );
     }
 
 
     private void PlayGame() {
         Score = (int)UnityEngine.Random.Range( 0f, 100f );
+        ReportScore();
     }
 
 
+    private void ReportScore() {
+        // TODO: Call to report score!
+        // ... and set this as the callback: ResetPolling();
+    }
+
+    
+    private void ResetPolling() {
+        Debug.LogWarning("ttt Start polling for results??");
+        // TODO: Start polling for the previous game states
+    }
 }

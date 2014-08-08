653e029b147e55e157fad37477959c02ea61cc7d
Andy Zinsser <andyznssr@gmail.com>
2014-08-08 10:50:25 -0700

    Create ArbiterAlertView class

---
+++ ExampleApp/Assets/Editor/Arbiter/Arbiter/ArbiterAlertView.h
@@ -0,0 +1,27 @@
+//
+//  ArbiterAlertView.h
+//  Unity-iPhone
+//
+//  Created by Andy Zinsser on 8/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "Arbiter.h"
+
+@interface ArbiterAlertView : UIView <NSURLConnectionDataDelegate>
+
+
+@property (assign) Arbiter *arbiter;
+@property (assign) NSMutableData *responseData;
+@property (assign) void(^responseHandler)(NSDictionary *responseDict);
+@property (assign) void(^callback)(void);
+
+- (id)initWithFrame:(CGRect)frame andCallback:(void(^)(void))callback arbiterInstance:(Arbiter *)arbiterInstance;
+- (void)setupNextScreen;
+- (void)animateIn;
+- (void)animateOut;
+- (void)cancelButtonClicked:(id)sender;
+- (NSString *)addThousandsSeparatorToString:(NSString *)original;
+
+@end
---
+++ ExampleApp/Assets/Editor/Arbiter/Arbiter/ArbiterAlertView.m
@@ -0,0 +1,164 @@
+//
+//  ArbiterAlertView.m
+//  Unity-iPhone
+//
+//  Created by Andy Zinsser on 8/8/14.
+//
+//
+
+#import "ArbiterAlertView.h"
+
+@implementation ArbiterAlertView
+{
+    CGRect *parentFrame;
+}
+
+- (id)initWithFrame:(CGRect)frame andCallback:(void(^)(void))handler arbiterInstance:(Arbiter *)arbiterInstance
+{
+    self = [super init];
+    
+    if (self) {
+        parentFrame = &(frame);
+        self.arbiter = arbiterInstance;
+        self.callback = handler;
+        
+        [self setFrameDimensions];
+        
+        [self setBackgroundColor:[[UIColor whiteColor] colorWithAlphaComponent:0.95f]];
+        [self.layer setCornerRadius:5.0f];
+        [self.layer setShadowColor:[UIColor blackColor].CGColor];
+        [self.layer setShadowOpacity:0.8];
+        [self.layer setShadowRadius:3.0];
+        [self.layer setShadowOffset:CGSizeMake(2.0, 2.0)];
+        
+        [self animateIn];
+        [self setupNextScreen];
+    }
+    return self;
+}
+
+- (void)setFrameDimensions
+{
+    float maxWidth = 420.0f;
+    float maxHeight = 285.0f;
+    CGRect frame = *(parentFrame);
+    
+    if ( frame.size.height > maxHeight ) {
+        frame.size.height = maxHeight;
+    }
+    
+    if ( frame.size.width > maxWidth ) {
+        frame.size.width = maxWidth;
+    }
+    
+    frame.size.width -= 25.0f;
+    frame.size.height -= 25.0f;
+    frame.origin.x = ([UIScreen mainScreen].bounds.size.width - frame.size.width) / 2;
+    frame.origin.y = ([UIScreen mainScreen].bounds.size.height - frame.size.height) / 2;
+    
+    [self setFrame:frame];
+}
+
+- (void)setupNextScreen
+{
+    NSLog(@"Override setupNextScreen in subclass");
+}
+
+# pragma mark Click Handlers
+
+- (void)cancelButtonClicked:(id)sender
+{
+    [self animateOut];
+    [self endEditing:YES];
+}
+
+# pragma mark AlertView Esqueue Animations
+
+- (void)animateIn
+{
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation
+                                      animationWithKeyPath:@"transform"];
+    
+    CATransform3D scale1 = CATransform3DMakeScale(0.5, 0.5, 1);
+    CATransform3D scale2 = CATransform3DMakeScale(0.9, 0.9, 1);
+    CATransform3D scale3 = CATransform3DMakeScale(1.1, 1.1, 1);
+    CATransform3D scale4 = CATransform3DMakeScale(1.0, 1.0, 1);
+    
+    NSArray *frameValues = [NSArray arrayWithObjects:
+                            [NSValue valueWithCATransform3D:scale1],
+                            [NSValue valueWithCATransform3D:scale2],
+                            [NSValue valueWithCATransform3D:scale3],
+                            [NSValue valueWithCATransform3D:scale4],
+                            nil];
+    [animation setValues:frameValues];
+    
+    NSArray *frameTimes = [NSArray arrayWithObjects:
+                           [NSNumber numberWithFloat:0.0],
+                           [NSNumber numberWithFloat:0.5],
+                           [NSNumber numberWithFloat:0.9],
+                           [NSNumber numberWithFloat:1.0],
+                           nil];
+    [animation setKeyTimes:frameTimes];
+    
+    animation.fillMode = kCAFillModeForwards;
+    animation.removedOnCompletion = NO;
+    animation.duration = .2;
+    
+    [self.layer addAnimation:animation forKey:@"popup"];
+}
+
+- (void)animateOut
+{
+    [UIView animateWithDuration:0.2f
+                     animations:^{ [self setAlpha:0.0f]; }
+                     completion:^(BOOL finished) { self.callback(); }];
+}
+
+# pragma mark NSURLConnection Delegate Methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    self.responseData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.responseData appendData:data];
+}
+
+- (NSCachedURLResponse *)connection:(NSURLConnection *)connection
+                  willCacheResponse:(NSCachedURLResponse*)cachedResponse {
+    return nil;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSError *error = nil;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:self.responseData
+                                                         options:NSJSONReadingMutableLeaves
+                                                           error:&error];
+    self.responseHandler(dict);
+    self.responseData = nil;
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    NSLog(@"Connection Error");
+}
+
+#pragma mark Utility Helpers
+
+- (NSString *)addThousandsSeparatorToString:(NSString *)original
+{
+    NSNumberFormatter *separatorFormattor = [[NSNumberFormatter alloc] init];
+    [separatorFormattor setFormatterBehavior: NSNumberFormatterBehavior10_4];
+    [separatorFormattor setNumberStyle: NSNumberFormatterDecimalStyle];
+    
+    NSNumberFormatter *stringToNumberFormatter = [[NSNumberFormatter alloc] init];
+    [stringToNumberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber *origNumber = [stringToNumberFormatter numberFromString:original];
+    
+    return [separatorFormattor stringFromNumber:origNumber];
+}
+
+
+
+
+@end

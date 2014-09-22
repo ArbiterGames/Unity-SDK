367389c62b8b459072a2914d60b73d2f548527bc
Andy Zinsser <andyznssr@gmail.com>
2014-09-22 16:27:04 -0700

    Get panel view animations consistent on 8 and 7

--- ExampleApp/Assets/Editor/Arbiter/Arbiter/Views/ArbiterPanelWindow.m
+++ ExampleApp/Assets/Editor/Arbiter/Arbiter/Views/ArbiterPanelWindow.m
@@ -27,28 +27,30 @@
 - (void)show:(ArbiterPanelView *)view
 {
     [self makeKeyAndVisible];
+    view.parentWindow = self;
     
     UIImage *image;
     UIGraphicsBeginImageContext(self.gameWindow.rootViewController.view.bounds.size);
-    [self.gameWindow.rootViewController.view drawViewHierarchyInRect:self.gameWindow.rootViewController.view.bounds afterScreenUpdates:true];
+    [self.gameWindow.rootViewController.view drawViewHierarchyInRect:self.gameWindow.rootViewController.view.bounds afterScreenUpdates:false];
     image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
-    view.parentWindow = self;
     self.rootViewController.view.backgroundColor = [UIColor colorWithPatternImage:[image applyDarkEffect]];
     [self.rootViewController.view addSubview:view];
-
+    
+    BOOL IS_LESS_THAN_IOS8 = [[[UIDevice currentDevice] systemVersion] compare: @"7.9" options: NSNumericSearch] != NSOrderedDescending;
+    BOOL IS_LANDSCAPE = UIDeviceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]);
     CGRect temp = self.frame;
-    if ( UIDeviceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]) ) {
-        temp.origin.x = temp.size.height;
-        
+    
+    if ( IS_LESS_THAN_IOS8 && IS_LANDSCAPE ) {
+        temp.origin.y = temp.size.width;
     } else {
         temp.origin.x = temp.size.width;
     }
-
+    
     self.rootViewController.view.frame = temp;
     temp.origin.y = 0.0;
     temp.origin.x = 0.0;
+    self.hidden = NO;
     [UIView animateWithDuration:0.3
                           delay:0.0
                         options: UIViewAnimationCurveEaseOut
@@ -56,15 +58,17 @@
                          self.rootViewController.view.frame = temp;
                      }
                      completion:nil];
-
+    
     [self renderPoweredBy];
 }
 
 - (void)hide
 {
     CGRect temp = self.rootViewController.view.frame;
-    if ( UIDeviceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]) ) {
-        temp.origin.x = temp.size.height;
+    BOOL IS_LESS_THAN_IOS8 = [[[UIDevice currentDevice] systemVersion] compare: @"7.9" options: NSNumericSearch] != NSOrderedDescending;
+    BOOL IS_LANDSCAPE = UIDeviceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]);
+    if ( IS_LESS_THAN_IOS8 && IS_LANDSCAPE ) {
+        temp.origin.y = temp.size.width;
     } else {
         temp.origin.x = temp.size.width;
     }
@@ -73,7 +77,9 @@
                         options: UIViewAnimationCurveEaseOut
                      animations:^{
                          self.rootViewController.view.frame = temp;
-                     }completion:^(BOOL finished){
+                     }
+                     completion:^(BOOL finished){
+                         self.hidden = YES;
                          for ( UIWindow *window in [[UIApplication sharedApplication] windows] ) {
                              for ( UIView *view in [self.rootViewController.view subviews] ) {
                                  [view removeFromSuperview];

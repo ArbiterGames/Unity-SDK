d1de8ce8f44f1adf73e1aac15202428c614bef3a
Andy Zinsser <andyznssr@gmail.com>
2014-10-22 11:17:54 -0700

    Put PassKit back since stripe requires it

--- UnityExample/Assets/Editor/ArbiterPostprocessor.py
+++ UnityExample/Assets/Editor/ArbiterPostprocessor.py
@@ -21,6 +21,7 @@ lib_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platfo
 # Add required libraries
 ############################
 project.add_file(frameworks_path + 'Security.framework', tree='SDKROOT')
+project.add_file(frameworks_path + 'PassKit.framework', tree='SDKROOT', weak=True)
 project.add_file(lib_path + 'libicucore.dylib', tree='SDKROOT')
 
 # Add all files in /Assets/Editor/Arbiter/

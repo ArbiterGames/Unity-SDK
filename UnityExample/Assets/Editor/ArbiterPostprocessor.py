2b4bc174020821963c819672350b2aca0445efd6
Andy Zinsser <andyznssr@gmail.com>
2014-10-22 11:03:10 -0700

    Remove PassKit import

--- UnityExample/Assets/Editor/ArbiterPostprocessor.py
+++ UnityExample/Assets/Editor/ArbiterPostprocessor.py
@@ -11,13 +11,16 @@ path = argv[1]
 fileToAddPath = argv[2]
 project = XcodeProject.Load(path + '/Unity-iPhone.xcodeproj/project.pbxproj')
 frameworks_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/'
+
+# TODO:
+#   need to figure out the best approach for checking which SDK the app is being developed on, and then look for that
+#   instead of pulling from iPhoneOS.sdk
 lib_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/'
 
 
 # Add required libraries
 ############################
 project.add_file(frameworks_path + 'Security.framework', tree='SDKROOT')
-project.add_file(frameworks_path + 'PassKit.framework', tree='SDKROOT', weak=True)
 project.add_file(lib_path + 'libicucore.dylib', tree='SDKROOT')
 
 # Add all files in /Assets/Editor/Arbiter/

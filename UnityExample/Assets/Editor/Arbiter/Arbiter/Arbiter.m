60fc0f3beba84b498f2dd084a2c648819f62c57c
August Zinsser <august.zinsser@gmail.com>
2014-12-31 12:07:25 -0800

    Cleanup

--- UnityExample/Assets/Editor/Arbiter/Arbiter/Arbiter.m
+++ UnityExample/Assets/Editor/Arbiter/Arbiter/Arbiter.m
@@ -164,12 +164,9 @@ static Arbiter *_sharedInstance = nil;
 
 - (void)loginAsAnonymous:(void(^)(NSDictionary *))handler
 {
-    // ttt TODO: Verify this block reads the response from the server correctly once the new code is deployed
     void (^connectionHandler)(NSDictionary *) = [^(NSDictionary *responseDict) {
         NSNumber* successObj = [responseDict objectForKey:@"success"];
-        NSLog(@"ttt succesObj=%@",successObj);
         if( successObj != nil && [successObj boolValue] == YES ) {
-            NSLog(@"ttt successful!");
             self.wallet = [NSMutableDictionary dictionaryWithDictionary:[responseDict objectForKey:@"wallet"]];
             self.user = [NSMutableDictionary dictionaryWithDictionary:[responseDict objectForKey:@"user"]];
             [[ARBTracking arbiterInstance] identify:[self.user objectForKey:@"id"]];    
@@ -179,10 +176,7 @@ static Arbiter *_sharedInstance = nil;
     
     if ( self.hasConnection ) {
         // Check to see if a previously-anonymous user token was saved. If so, pass that along so the server doesn't create a new user
-        // ttt TODO: this is the correct way:
         NSString* savedToken = [[NSUserDefaults standardUserDefaults] objectForKey:DEFAULTS_USER_TOKEN];
-        //NSString* savedToken = @"arbitrary fake token";
-        NSLog(@"ttt previously saved token=%@", savedToken);
         if ( !IS_NULL_NS(savedToken)) {
             self.user = [[NSMutableDictionary alloc] initWithDictionary:@{USER_TOKEN:[NSString stringWithString:savedToken]}];
         }
@@ -915,10 +909,8 @@ static Arbiter *_sharedInstance = nil;
     if ( !IS_NULL_NS([self.user objectForKey:USER_TOKEN]) ) {
         tokenValue = [NSString stringWithFormat:@"Token %@::%@", [self.user objectForKey:USER_TOKEN], self.apiKey];
     } else {
-        NSLog(@"ttt using accessToken...");
         tokenValue = [NSString stringWithFormat:@"Token %@::%@", self.accessToken, self.apiKey];
     }
-    NSLog(@"ttt token=%@", tokenValue);
     
     [request setHTTPShouldHandleCookies:NO];
     [request setValue:tokenValue forHTTPHeaderField:@"Authorization"];

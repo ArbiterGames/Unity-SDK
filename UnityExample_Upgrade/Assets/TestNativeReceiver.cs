b74926277a74c67eab50953420c8b2797ba0d32a
August Zinsser <august.zinsser@gmail.com>
2015-07-18 16:08:17 -0700

    adding token and api key

--- UnityExample_Upgrade/Assets/TestNativeReceiver.cs
+++ UnityExample_Upgrade/Assets/TestNativeReceiver.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
+
 
 public class TestNativeReceiver : MonoBehaviour {
+
+	public string Token;
+	public string ApiKey;
+
+	
+	void Start() {
+		_init( Token, ApiKey );
+	}
+
 
 	public void SimpleUnityFunction( string param ) {
 		Debug.Log( "Simple Unity Function. Param="+param );
 	}
 
+
+	
+	[DllImport("__Internal")]
+	extern static public void _init( string apiKey, string token );
 }

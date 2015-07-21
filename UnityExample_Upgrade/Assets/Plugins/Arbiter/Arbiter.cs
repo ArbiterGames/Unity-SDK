84321c33f9f8c629efbf4514b439891cff8621e5
August Zinsser <august.zinsser@gmail.com>
2015-07-20 17:36:43 -0700

    Checkpoint commit

--- UnityExample_Upgrade/Assets/Plugins/Arbiter/Arbiter.cs
+++ UnityExample_Upgrade/Assets/Plugins/Arbiter/Arbiter.cs
@@ -3,22 +3,18 @@ using System.Collections;
 using System.Runtime.InteropServices;
 
 
-public class TestNativeReceiver : MonoBehaviour {
+
+public class Arbiter : MonoBehaviour {
+
 
 	public string Token;
 	public string ApiKey;
 
 	
-	void Start() {
+	void Start () {
 		_init( ApiKey, Token );
 	}
-
-
-	public void SimpleUnityFunction( string param ) {
-		Debug.Log( "Simple Unity Function. Param="+param );
-	}
-
-
+	
 	
 	[DllImport("__Internal")]
 	extern static public void _init( string apiKey, string token );

e9120a1d0d145af44eed480b96acc013f0f9df16
August Zinsser <august.zinsser@gmail.com>
2015-07-26 17:21:26 -0700

    Adding debug logs

--- UnityExample_Upgrade/Assets/LastErrorField.cs
+++ UnityExample_Upgrade/Assets/LastErrorField.cs
@@ -12,14 +12,19 @@ public class LastErrorField : DynamicTextField {
 
 	public static void ShowGlobalError( List<string> messages ) {
 		Debug.Log ("ttt in ShowGlobalError");
+		Debug.Log (messages);
 		if( messages != null && messages.Count > 0 ) {
+			Debug.Log ("ttt in Check 1");
+			Debug.Log (messages[0]);
 			ShowGlobalError( messages[0] );
 		}
 	}
 
 
 	public static void ShowGlobalError( string message ) {
+		Debug.Log ("ttt in Check 2");
 		if( instance != null && instance.gameObject != null ) {
+			Debug.Log ("ttt in Check 3");
 			instance.SetText( message );
 		}
 	}
